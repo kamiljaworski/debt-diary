63069a8ec254364db2f0fcc60c93843cebf90069
Kamil Jaworski <kamilj610@gmail.com>
2018-06-29 01:29:49 +0200

    Added Fody PropertyChanged and Microsoft.Windows.Shell NuGet Packages. Created MainWindowViewModel and a little bit window styling.

---
+++ DebtDiary/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DebtDiary
+{
+    /// <summary>
+    /// Main Window View Model
+    /// </summary>
+    public class MainWindowViewModel : INotifyPropertyChanged
+    {
+        #region INotifyPropertyChanged implementation
+        // Doing nothing lambda expression to avoid compiler warning
+        public event PropertyChangedEventHandler PropertyChanged = (sender, e) => { };
+        #endregion
+
+        #region Public Properties
+        // Minimum width the window can get +4 because of dropshadow and 2pt margin
+        public int MinimumWidth { get; set; } = 1284;
+
+        // Minimum height the window can get +4 because of dropshadow and 2pt margin
+        public int MinimumHeight { get; set; } = 724;
+
+        public int TitleBarHeight { get; set; } = 35;
+        #endregion
+
+    }
+}
