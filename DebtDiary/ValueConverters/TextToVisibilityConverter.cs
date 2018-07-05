43b7b8d5cb844ab6148001944a5de191888bd6d7
Kamil Jaworski <kamilj610@gmail.com>
2018-07-05 21:52:41 +0200

    Some xaml styling

---
+++ DebtDiary/ValueConverters/TextToVisibilityConverter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+
+namespace DebtDiary
+{
+    /// <summary>
+    /// Converts textbox text to visibility
+    /// </summary>
+    public class TextToVisibilityConverter : BaseValueConverter<TextToVisibilityConverter>
+    {
+        public override object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (string.IsNullOrEmpty((string)value))
+                return Visibility.Visible;
+            return Visibility.Hidden;
+        }
+
+        public override object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) => throw new NotImplementedException();
+    }
+}
