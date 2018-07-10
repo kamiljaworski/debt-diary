109794c027cdd406fb9b3970e80d95c6e167736a
Kamil Jaworski <kamilj610@gmail.com>
2018-07-10 16:58:14 +0200

    Added Attached Property for a PasswordBox to make hover text

--- DebtDiary/ValueConverters/BolleanToVisibilityConverter.cs
+++ DebtDiary/ValueConverters/BolleanToVisibilityConverter.cs
@@ -6,13 +6,13 @@ using System.Windows;
 namespace DebtDiary
 {
     /// <summary>
-    /// Converts text of a TextBox to Visibility
+    /// Converts Bollean of a PasswordBox attached property to Visibility
     /// </summary>
-    public class TextToVisibilityConverter : BaseValueConverter<TextToVisibilityConverter>
+    public class BolleanToVisibilityConverter : BaseValueConverter<BolleanToVisibilityConverter>
     {
         public override object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
-            if (value is String && String.IsNullOrEmpty((value as String)))
+            if (value is Boolean && (Boolean)value == false)
                 return Visibility.Visible;
             return Visibility.Hidden;
         }
