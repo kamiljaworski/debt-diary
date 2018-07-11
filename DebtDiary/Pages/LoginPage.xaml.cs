3ff3176c9a3ccf25a8970579de11d8fdedf88f6f
Kamil Jaworski <kamilj610@gmail.com>
2018-07-11 16:27:59 +0200

    Moved MainWindow content into LoginPage

---
+++ DebtDiary/Pages/LoginPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace DebtDiary
+{
+    /// <summary>
+    /// Interaction logic for LoginPage.xaml
+    /// </summary>
+    public partial class LoginPage : Page
+    {
+        public LoginPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
