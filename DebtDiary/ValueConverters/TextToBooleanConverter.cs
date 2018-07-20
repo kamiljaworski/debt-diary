6abe7e93d16fc4018a1b3192eb64ed621b03aad5
Kamil Jaworski <kamilj610@gmail.com>
2018-07-20 18:50:31 +0200

    Add TextToBolleanConverter

---
+++ DebtDiary/ValueConverters/TextToBooleanConverter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+
+namespace DebtDiary
+{
+    /// <summary>
+    /// Converts text of a TextBox to Boolean value
+    /// </summary>
+    public class TextToBooleanConverter : BaseValueConverter<TextToBooleanConverter>
+    {
+        public override object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is String && String.IsNullOrEmpty((value as String)))
+                return false;
+            return true;
+        }
+
+        public override object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) => throw new NotImplementedException();
+    }
+}
