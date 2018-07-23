d24b602a491e8fb66fd0922fc1413a431a4ea271
Kamil Jaworski <kamilj610@gmail.com>
2018-07-23 18:22:57 +0200

    Add DiaryPage to the project

---
+++ DebtDiary/Views/Pages/DiaryPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace DebtDiary
+{
+    /// <summary>
+    /// Interaction logic for DiaryPage.xaml
+    /// </summary>
+    public partial class DiaryPage : Page
+    {
+        public DiaryPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
