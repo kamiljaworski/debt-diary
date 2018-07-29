40f52a3d700aa4aef7d992707ecd829f2ccdd917
Kamil Jaworski <kamilj610@gmail.com>
2018-07-29 16:35:05 +0200

    WindowStateToIconConverter code cleanup

--- DebtDiary/ValueConverters/WindowStateToIconConverter.cs
+++ DebtDiary/ValueConverters/WindowStateToIconConverter.cs
@@ -11,9 +11,14 @@ namespace DebtDiary
     {
         public override object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
+            // Get icons from application resouce dictionary
+            string maximizeIcon = Application.Current.FindResource("MaximizeIcon") as string;
+            string restoreIcon = Application.Current.FindResource("RestoreIcon") as string;
+
+            // Return right icon
             if ((WindowState)value == WindowState.Maximized)
-                return "\uE923";
-            return "\uE922";
+                return maximizeIcon;
+            return restoreIcon;
         }
 
         public override object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) => throw new NotImplementedException();
