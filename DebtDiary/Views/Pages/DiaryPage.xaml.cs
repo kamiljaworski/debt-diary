c598a64a6885cbd0d37e5410e1a384f143e7698b
Kamil Jaworski <kamilj610@gmail.com>
2018-07-29 18:31:24 +0200

    Add temporary chart and last operations styling

--- DebtDiary/Views/Pages/DiaryPage.xaml.cs
+++ DebtDiary/Views/Pages/DiaryPage.xaml.cs
@@ -1,4 +1,8 @@
-﻿using System.Windows.Controls;
+﻿using LiveCharts;
+using LiveCharts.Wpf;
+using System;
+using System.Windows.Controls;
+using System.Windows.Media;
 
 namespace DebtDiary
 {
@@ -7,9 +11,27 @@ namespace DebtDiary
     /// </summary>
     public partial class DiaryPage : Page
     {
+        public SeriesCollection SeriesCollection { get; set; }
+        public string[] Labels { get; set; }
+        public Func<double, string> YFormatter { get; set; }
+
         public DiaryPage()
         {
             InitializeComponent();
+
+            SeriesCollection = new SeriesCollection
+            {
+                new LineSeries
+                {
+                    Title = "Test",
+                    Values = new ChartValues<double> { -1000, -700, 0, -200 , 0, 200, 500, 100, 0, 290 }
+                }
+            };
+
+            YFormatter = value => value.ToString("C");
+
+
+            DataContext = this;
         }
     }
 }
