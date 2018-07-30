25680dba66c2651384ee979b0846c422c8fc2a3c
Kamil Jaworski <kamilj610@gmail.com>
2018-07-30 17:35:51 +0200

    Add SecureStringExtensions class with GetPassword method

---
+++ DebtDiary.Core/Helpers/SecureStringExtensions.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Security;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DebtDiary.Core
+{
+    public static class SecureStringExtensions
+    {
+        public static string GetPassword(this SecureString secureString)
+        {
+            IntPtr value = IntPtr.Zero;
+            try
+            {
+                value = Marshal.SecureStringToGlobalAllocUnicode(secureString);
+                return Marshal.PtrToStringUni(value);
+            }
+            finally
+            {
+                Marshal.ZeroFreeGlobalAllocUnicode(value);
+            }
+        }
+    }
+}
