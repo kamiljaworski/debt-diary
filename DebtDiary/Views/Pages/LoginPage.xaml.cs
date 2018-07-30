5711e5d8abfc6817919d58d6a3ad497785bfa43b
Kamil Jaworski <kamilj610@gmail.com>
2018-07-30 17:53:21 +0200

    IHavePassword interface implementation in LoginPage

--- DebtDiary/Views/Pages/LoginPage.xaml.cs
+++ DebtDiary/Views/Pages/LoginPage.xaml.cs
@@ -1,16 +1,22 @@
-﻿using System.Windows.Controls;
+﻿using System.Security;
+using System.Windows.Controls;
 
 namespace DebtDiary
 {
     /// <summary>
     /// Interaction logic for LoginPage.xaml
     /// </summary>
-    public partial class LoginPage : Page
+    public partial class LoginPage : Page, IHavePassword
     {
         public LoginPage()
         {
             DataContext = new LoginPageViewModel();
             InitializeComponent();
         }
+
+        /// <summary>
+        /// IHavePassword interface implementation
+        /// </summary>
+        public SecureString Password => UserPassword.SecurePassword;
     }
 }
