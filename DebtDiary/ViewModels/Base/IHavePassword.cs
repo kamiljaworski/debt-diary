6511e06c123bca41704c734cd7d8589a47538682
Kamil Jaworski <kamilj610@gmail.com>
2018-07-30 17:50:25 +0200

    Add IHavePassword interface to the main project

---
+++ DebtDiary/ViewModels/Base/IHavePassword.cs
@@ -0,0 +1,15 @@
+﻿using System.Security;
+
+namespace DebtDiary
+{
+    /// <summary>
+    /// An interface for a class that wants to get Secure Password
+    /// </summary>
+    public interface IHavePassword
+    {
+        /// <summary>
+        /// The Secure Password
+        /// </summary>
+        SecureString Password { get; }
+    }
+}
