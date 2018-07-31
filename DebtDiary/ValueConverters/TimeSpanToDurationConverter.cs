c6421f7863eb37f504b326174c0d5868b8381f5c
Kamil Jaworski <kamilj610@gmail.com>
2018-07-31 18:48:17 +0200

    Delete ApplicationPage... and add TimeSpanToDurationConverter

---
+++ DebtDiary/ValueConverters/TimeSpanToDurationConverter.cs
@@ -0,0 +1,24 @@
+﻿using DebtDiary.Core;
+using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Media;
+
+namespace DebtDiary
+{
+    /// <summary>
+    /// Converts <see cref="TimeSpan"/> to <see cref="Duration"/> for xaml binding
+    /// </summary>
+    public class TimeSpanToDurationConverter : BaseValueConverter<TimeSpanToDurationConverter>
+    {
+        public override object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is TimeSpan)
+                return new Duration((TimeSpan)value);
+
+            return null;
+        }
+
+        public override object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) => throw new NotImplementedException();
+    }
+}
