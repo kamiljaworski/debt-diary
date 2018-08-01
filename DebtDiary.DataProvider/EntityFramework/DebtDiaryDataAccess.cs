f51bad0e82a348b208e61ddc9f8290f8d5a90dba
Kamil Jaworski <kamilj610@gmail.com>
2018-08-01 19:32:19 +0200

    Add DebtDiaryDataAccess class

---
+++ DebtDiary.DataProvider/EntityFramework/DebtDiaryDataAccess.cs
@@ -0,0 +1,34 @@
+﻿using DebtDiary.Core;
+using System;
+
+namespace DebtDiary.DataProvider
+{
+    /// <summary>
+    /// Application data access class which use Entity Framework database
+    /// </summary>
+    public class DebtDiaryDataAccess : IDebtDiaryDataAccess, IDisposable
+    {
+        /// <summary>
+        /// Entity Framework database context
+        /// </summary>
+        private DebtDiaryDbContext dbContext = new DebtDiaryDbContext();
+
+        /// <summary>
+        /// Register user method
+        /// </summary>
+        /// <param name="user">User you want to register in the database</param>
+        public void RegisterUser(User user)
+        {
+            dbContext.Users.Add(user);
+            dbContext.SaveChanges();
+        }
+
+        /// <summary>
+        /// IDisposable interface implementation method
+        /// </summary>
+        public void Dispose()
+        {
+            dbContext.Dispose();
+        }
+    }
+}
