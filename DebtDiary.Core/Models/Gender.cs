9d956d88496031e7b942d22699669c96d5670131
Kamil Jaworski <kamilj610@gmail.com>
2018-08-01 16:55:20 +0200

    Add Gender enum

---
+++ DebtDiary.Core/Models/Gender.cs
@@ -0,0 +1,11 @@
+﻿namespace DebtDiary.Core
+{
+    /// <summary>
+    /// Enum that describes gender of a person
+    /// </summary>
+    public enum Gender
+    {
+        Male = 1,
+        Female = 2
+    }
+}
