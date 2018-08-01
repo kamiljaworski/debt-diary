e58f1f8a0b930f62a182f94d7bfd694d704cd02b
Kamil Jaworski <kamilj610@gmail.com>
2018-08-01 19:28:27 +0200

    Add IDebtDiaryDataAccess interface

---
+++ DebtDiary.DataProvider/EntityFramework/IDebtDiaryDataAccess.cs
@@ -0,0 +1,16 @@
+﻿using DebtDiary.Core;
+
+namespace DebtDiary.DataProvider
+{
+    /// <summary>
+    /// Interface for data access
+    /// </summary>
+    public interface IDebtDiaryDataAccess
+    {
+        /// <summary>
+        /// Register user method
+        /// </summary>
+        /// <param name="user">User to register in the database</param>
+        void RegisterUser(User user);
+    }
+}
