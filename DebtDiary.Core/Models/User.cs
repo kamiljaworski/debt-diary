8e62d63c74fdcfead46add0e664d5ed7e30dcd50
Kamil Jaworski <kamilj610@gmail.com>
2018-08-01 18:12:10 +0200

    Add User class

---
+++ DebtDiary.Core/Models/User.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace DebtDiary.Core
+{
+    /// <summary>
+    /// User
+    /// </summary>
+    public class User
+    {
+        /// <summary>
+        /// Username
+        /// </summary>
+        public string Username { get; set; }
+
+        /// <summary>
+        /// User's first name
+        /// </summary>
+        public string FirstName { get; set; }
+
+        /// <summary>
+        /// User's last name
+        /// </summary>
+        public string LastName { get; set; }
+
+        /// <summary>
+        /// User's e-mail
+        /// </summary>
+        public string Email { get; set; }
+
+        /// <summary>
+        /// User's encrypted password
+        /// </summary>
+        public string Password { get; set; }
+
+        /// <summary>
+        /// User's gender
+        /// </summary>
+        public Gender? Gender { get; set; }
+
+        /// <summary>
+        /// User's register date
+        /// </summary>
+        public DateTime? RegisterDate { get; set; }
+
+    }
+}
