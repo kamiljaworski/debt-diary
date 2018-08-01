b671c2835cb5b0d4a847b36428eca12bae2184d3
Kamil Jaworski <kamilj610@gmail.com>
2018-08-01 18:29:48 +0200

    Add DebtDiaryDbContext

---
+++ DebtDiary.Core/Database/DebtDiaryDbContext.cs
@@ -0,0 +1,22 @@
+﻿using System.Data.Entity;
+
+namespace DebtDiary.Core
+{
+    /// <summary>
+    /// Application database context
+    /// </summary>
+    public class DebtDiaryDbContext : DbContext
+    {
+        /// <summary>
+        /// Default constructor
+        /// </summary>
+        public DebtDiaryDbContext(): base("name = DebtDiaryConnectionString")
+        {
+        }
+
+        /// <summary>
+        /// Users table
+        /// </summary>
+        public DbSet<User> Users { get; set; }
+    }
+}
