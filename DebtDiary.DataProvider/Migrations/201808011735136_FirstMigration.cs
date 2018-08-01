ac9bd572af40fd4bbeabc538a7930708af95e0e2
Kamil Jaworski <kamilj610@gmail.com>
2018-08-01 19:35:50 +0200

    Add migrations to the DataProvider project

---
+++ DebtDiary.DataProvider/Migrations/201808011735136_FirstMigration.cs
@@ -0,0 +1,32 @@
+namespace DebtDiary.DataProvider.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class FirstMigration : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Users",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Username = c.String(),
+                        FirstName = c.String(),
+                        LastName = c.String(),
+                        Email = c.String(),
+                        Password = c.String(),
+                        Gender = c.Int(),
+                        RegisterDate = c.DateTime(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Users");
+        }
+    }
+}
