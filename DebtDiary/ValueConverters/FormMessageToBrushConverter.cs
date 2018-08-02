daa39d7ac085eb909ba694c983cc1a5507b2d0d1
Kamil Jaworski <kamilj610@gmail.com>
2018-08-02 19:58:56 +0200

    Add colored textbox border when there is any form message

---
+++ DebtDiary/ValueConverters/FormMessageToBrushConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Media;
+
+namespace DebtDiary
+{
+    /// <summary>
+    /// Converts <see cref="FormMessage"/> to <see cref="Brush"/> for coloring border in textboxes when <see cref="FormMessage"/> is set
+    /// </summary>
+    public class FormMessageToBrushConverter : BaseValueConverter<FormMessageToBrushConverter>
+    {
+        public override object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            // If form message is set return red brush
+            if ((FormMessage)value != FormMessage.None)
+                return Brushes.Red;
+
+            // If not return null
+            return Application.Current.FindResource("DarkGreyBrush") as Brush;
+        }
+
+        public override object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) => throw new NotImplementedException();
+    }
+}
