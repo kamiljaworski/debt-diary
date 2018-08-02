2268935a685be25312ebb22da53cf7d2db9bd819
Kamil Jaworski <kamilj610@gmail.com>
2018-08-02 18:28:03 +0200

    Add FormMessages enum

---
+++ DebtDiary/ViewModels/Application/FormMessages.cs
@@ -0,0 +1,17 @@
+﻿namespace DebtDiary.ViewModels.Application
+{
+    /// <summary>
+    /// Enum that describes message that will be shown in the application forms
+    /// if there is any problem with any field in the form
+    /// </summary>
+    public enum FormMessages
+    {
+        None,
+        EmptyFirstName,
+        EmptyLastName,
+        EmptyUsername,
+        EmptyEmail,
+        EmptyPassword,
+        EmptyRepeatedPassword
+    }
+}
