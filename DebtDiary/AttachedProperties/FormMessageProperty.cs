556341a8dc3e2c840a3ace3fd3793ff2a872a7ed
Kamil Jaworski <kamilj610@gmail.com>
2018-08-03 21:44:35 +0200

    Add FormMessage attached property and first implementation in RegisterPage

---
+++ DebtDiary/AttachedProperties/FormMessageProperty.cs
@@ -0,0 +1,9 @@
+﻿namespace DebtDiary
+{
+    /// <summary>
+    /// FormMessage attached property used for showing messages
+    /// </summary>
+    class FormMessageProperty : BaseAttachedProperty<FormMessageProperty, FormMessage>
+    {
+    }
+}
