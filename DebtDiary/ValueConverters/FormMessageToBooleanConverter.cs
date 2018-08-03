9066c802b983f77b5208fa6d728b8fe7e144a911
Kamil Jaworski <kamilj610@gmail.com>
2018-08-03 22:41:31 +0200

    Add working error message animations to TextBox

---
+++ DebtDiary/ValueConverters/FormMessageToBooleanConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+
+namespace DebtDiary
+{
+    /// <summary>
+    /// Converts <see cref="FormMessage"/> to <see cref="Boolean"/> for starting xaml animations
+    /// </summary>
+    public class FormMessageToBooleanConverter : BaseValueConverter<FormMessageToBooleanConverter>
+    {
+        public override object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            // If there is no form message return false
+            if ((FormMessage)value == FormMessage.None)
+                return false;
+
+            // If there is return true
+            return true;
+        }
+
+        public override object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) => throw new NotImplementedException();
+    }
+}
