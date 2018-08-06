0398e500febd5b34f201a1c6d64f53b1d1156142
Kamil Jaworski <kamilj610@gmail.com>
2018-08-06 18:50:04 +0200

    Add IsBusy attached property

---
+++ DebtDiary/AttachedProperties/IsBusyProperty.cs
@@ -0,0 +1,9 @@
+﻿namespace DebtDiary
+{
+    /// <summary>
+    /// IsBusy attached property used for blocking buttons and showing spinning text inside them
+    /// </summary>
+    class IsBusyProperty : BaseAttachedProperty<IsBusyProperty, bool>
+    {
+    }
+}
