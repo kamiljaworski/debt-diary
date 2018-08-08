2504baf53bbc7eb9f9323ca12ce0fa0e53c16f94
Kamil Jaworski <kamilj610@gmail.com>
2018-08-08 19:06:19 +0200

    Add IApplicationViewModel interface

---
+++ DebtDiary/ViewModels/Application/IApplicationViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace DebtDiary
+{
+    /// <summary>
+    /// Interface for an ApplicationViewModel class
+    /// </summary>
+    public interface IApplicationViewModel
+    {
+        /// <summary>
+        /// Current page in the application
+        /// </summary>
+        ApplicationPage CurrentPage { get; }
+
+        /// <summary>
+        /// Page fade in animation duration
+        /// </summary>
+        TimeSpan FadeInDuration { get; set; }
+
+        /// <summary>
+        /// Page fade out animation duration
+        /// </summary>
+        TimeSpan FadeOutDuration { get; set; }
+
+        /// <summary>
+        /// Changes current page in the application
+        /// </summary>
+        /// <param name="page"><see cref="ApplicationPage"/> you want to change to</param>
+        void ChangeCurrentPage(ApplicationPage page);
+    }
+}
