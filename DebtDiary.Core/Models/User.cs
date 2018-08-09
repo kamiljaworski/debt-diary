b83ab28ead8b444c7043d263003b6c91553aaa88
Kamil Jaworski <kamilj610@gmail.com>
2018-08-09 18:59:09 +0200

    Add Initials property to user class and remove documentation

--- DebtDiary.Core/Models/User.cs
+++ DebtDiary.Core/Models/User.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
 
 namespace DebtDiary.Core
 {
@@ -9,52 +10,38 @@ namespace DebtDiary.Core
     /// </summary>
     public class User
     {
-        /// <summary>
-        /// User's id
-        /// </summary>
         public int Id { get; set; }
 
-        /// <summary>
-        /// Username
-        /// </summary>
         [StringLength(80)]
-        [Index(IsUnique=true)]
+        [Index(IsUnique = true)]
         public string Username { get; set; }
 
-        /// <summary>
-        /// User's first name
-        /// </summary>
         [StringLength(80)]
         public string FirstName { get; set; }
 
-        /// <summary>
-        /// User's last name
-        /// </summary>
         [StringLength(80)]
         public string LastName { get; set; }
 
-        /// <summary>
-        /// User's e-mail
-        /// </summary>
         [StringLength(80)]
-        [Index(IsUnique=true)]
+        [Index(IsUnique = true)]
         public string Email { get; set; }
 
-        /// <summary>
-        /// User's encrypted password
-        /// </summary>
         [StringLength(256)]
         public string Password { get; set; }
 
-        /// <summary>
-        /// User's gender
-        /// </summary>
         public Gender? Gender { get; set; }
 
-        /// <summary>
-        /// User's register date
-        /// </summary>
         public DateTime? RegisterDate { get; set; }
 
+        public string Initials
+        {
+            get
+            {
+                StringBuilder stringBuilder = new StringBuilder();
+                stringBuilder.Append(FirstName[0], 1);
+                stringBuilder.Append(LastName[0], 1);
+                return stringBuilder.ToString();
+            }
+        }
     }
 }
