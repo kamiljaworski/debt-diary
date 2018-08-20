71ae030f4737562e64e4d5ed1e96f018d855e123
Kamil Jaworski <kamilj610@gmail.com>
2018-08-20 19:13:34 +0200

    Add full dialog window binding

---
+++ DebtDiary/ValueConverters/DialogMessageToBrushConverter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Media;
+
+namespace DebtDiary
+{
+    /// <summary>
+    /// Converts <see cref="DialogMessage"/> to <see cref="Brush"/> for icon color in dialog messages
+    /// </summary>
+    public class DialogMessageToBrushConverter : BaseValueConverter<DialogMessageToBrushConverter>
+    {
+        public override object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            // If value is a DialogMessage return appropriate color
+            if (value is DialogMessage)
+                switch ((DialogMessage)value)
+                {
+                    case DialogMessage.None:
+                        return Brushes.Red;
+
+                    case DialogMessage.AccountCreated:
+                        return Brushes.Green;
+                }
+
+            // If not return green
+            return Brushes.Green;
+        }
+
+        public override object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) => throw new NotImplementedException();
+    }
+}
---
+++ DebtDiary/ValueConverters/DialogMessageToIconConverter.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+
+namespace DebtDiary
+{
+    /// <summary>
+    /// Converts <see cref="DialogMessage"/> to icon like: "&#xE922;"
+    /// </summary>
+    public class DialogMessageToIconConverter : BaseValueConverter<DialogMessageToIconConverter>
+    {
+        public override object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            try
+            {
+                // Get icons from application resouce dictionary
+                string successIcon = Application.Current.FindResource("SuccessIcon") as string;
+                string failureIcon = Application.Current.FindResource("FailureIcon") as string;
+
+                // If value is a DialogMessage return appropriate icon
+                if (value is DialogMessage)
+                    switch ((DialogMessage)value)
+                    {
+                        case DialogMessage.None:
+                            return failureIcon;
+
+                        case DialogMessage.AccountCreated:
+                            return successIcon;                            
+                    }
+
+                // If not return success icon
+                return successIcon;
+            }
+            catch
+            {
+                return null;
+            }
+        }
+
+        public override object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) => throw new NotImplementedException();
+    }
+}
