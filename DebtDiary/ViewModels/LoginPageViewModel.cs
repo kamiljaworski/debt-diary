ef92440378bb9310e8bd7905da838039db1b5b7a
Kamil Jaworski <kamilj610@gmail.com>
2018-08-20 17:32:38 +0200

    Add temporary dialog window opening to LoginPage

--- DebtDiary/ViewModels/LoginPageViewModel.cs
+++ DebtDiary/ViewModels/LoginPageViewModel.cs
@@ -47,10 +47,12 @@ namespace DebtDiary
         /// </summary>
         private void Login(object parameter)
         {
-            string username = Username;
-            string password = (parameter as IHavePassword).Password.GetEncryptedPassword();
+            //string username = Username;
+            //string password = (parameter as IHavePassword).Password.GetEncryptedPassword();
 
-            ChangeApplicationPage(ApplicationPage.DiaryPage);
+            //ChangeApplicationPage(ApplicationPage.DiaryPage);
+
+            var result = IocContainer.Get<IApplicationViewModel>().OpenDialog();
         }
         #endregion
     }
