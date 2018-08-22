bc5ad2e042de1821abf6534854db79aaef809bed
Kamil Jaworski <kamilj610@gmail.com>
2018-08-22 21:31:47 +0200

    Add Person class to remove repeatable code

---
+++ DebtDiary.Core/Models/Person.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace DebtDiary.Core
+{
+    /// <summary>
+    /// Abstract Person class
+    /// </summary>
+    public abstract class Person
+    {
+        [StringLength(80)]
+        public string FirstName { get; set; }
+
+        [StringLength(80)]
+        public string LastName { get; set; }
+
+        public Gender? Gender { get; set; }
+
+        public string Initials
+        {
+            get
+            {
+                StringBuilder stringBuilder = new StringBuilder();
+                stringBuilder.Append(FirstName[0], 1);
+                stringBuilder.Append(LastName[0], 1);
+                return stringBuilder.ToString().ToUpper();
+            }
+        }
+    }
+}
--- DebtDiary.Core/Models/User.cs
+++ DebtDiary.Core/Models/User.cs
@@ -8,19 +8,13 @@ namespace DebtDiary.Core
     /// <summary>
     /// User
     /// </summary>
-    public class User
+    public class User : Person
     {
         public int Id { get; set; }
 
         [StringLength(80)]
         [Index(IsUnique = true)]
         public string Username { get; set; }
-
-        [StringLength(80)]
-        public string FirstName { get; set; }
-
-        [StringLength(80)]
-        public string LastName { get; set; }
 
         [StringLength(80)]
         [Index(IsUnique = true)]
@@ -31,19 +25,7 @@ namespace DebtDiary.Core
 
         public DateTime? RegisterDate { get; set; }
 
-        public Gender? Gender { get; set; }
-
         public AvatarColor AvatarColor { get; set; } = AvatarColor.Green;
 
-        public string Initials
-        {
-            get
-            {
-                StringBuilder stringBuilder = new StringBuilder();
-                stringBuilder.Append(FirstName[0], 1);
-                stringBuilder.Append(LastName[0], 1);
-                return stringBuilder.ToString().ToUpper();
-            }
-        }
     }
 }
