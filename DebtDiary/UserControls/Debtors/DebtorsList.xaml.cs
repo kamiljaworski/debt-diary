938a66d773012499619b592a8e30b7541b6b007b
Kamil Jaworski <kamilj610@gmail.com>
2018-08-22 17:37:21 +0200

    Add DebtorsList user control

---
+++ DebtDiary/UserControls/Debtors/DebtorsList.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace DebtDiary
+{
+    /// <summary>
+    /// Interaction logic for DebtorsList.xaml
+    /// </summary>
+    public partial class DebtorsList : UserControl
+    {
+        public DebtorsList()
+        {
+            InitializeComponent();
+        }
+    }
+}
