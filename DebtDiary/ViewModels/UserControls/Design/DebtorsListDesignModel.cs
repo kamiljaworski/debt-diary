326bfef8732c04dd18c3a25b616109722e8be778
Kamil Jaworski <kamilj610@gmail.com>
2018-08-22 18:13:10 +0200

    Some DebtorsList restyling

--- DebtDiary/ViewModels/UserControls/Design/DebtorsListDesignModel.cs
+++ DebtDiary/ViewModels/UserControls/Design/DebtorsListDesignModel.cs
@@ -45,6 +45,41 @@ namespace DebtDiary
                     Initials = "JD",
                     Debt = 999.00m,
                     AvatarColor = AvatarColor.LightSeaGreen
+               },
+                               new DebtorsListItemViewModel
+                {
+                    FullName = "Kamil Jaworski",
+                    Initials = "KJ",
+                    Debt = -190.00m,
+                    AvatarColor = AvatarColor.Green
+               },
+               new DebtorsListItemViewModel
+               {
+                    FullName = "Jan Kowalski",
+                    Initials = "JK",
+                    Debt = 200.00m,
+                    AvatarColor = AvatarColor.Orange
+               },
+               new DebtorsListItemViewModel
+               {
+                    FullName = "Adam Nowak",
+                    Initials = "AN",
+                    Debt = 9999.00m,
+                    AvatarColor = AvatarColor.LightSeaGreen
+               },
+               new DebtorsListItemViewModel
+               {
+                    FullName = "Marek Maślana",
+                    Initials = "MM",
+                    Debt = 0.00m,
+                    AvatarColor = AvatarColor.Green
+               },
+               new DebtorsListItemViewModel
+               {
+                    FullName = "Jacek Dzięcioł",
+                    Initials = "JD",
+                    Debt = 999.00m,
+                    AvatarColor = AvatarColor.LightSeaGreen
                }
             };
         }
