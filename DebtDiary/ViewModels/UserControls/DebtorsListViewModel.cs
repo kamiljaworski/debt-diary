388180f92e480b0dcb4402a890e86f7f2ab16a4a
Kamil Jaworski <kamilj610@gmail.com>
2018-08-22 17:48:54 +0200

    Add DebtorsList view model and its design time data

---
+++ DebtDiary/ViewModels/UserControls/DebtorsListViewModel.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace DebtDiary
+{
+    public class DebtorsListViewModel : BaseViewModel
+    {
+        public List<DebtorsListItemViewModel> Debtors { get; set; }
+    }
+}
---
+++ DebtDiary/ViewModels/UserControls/Design/DebtorsListDesignModel.cs
@@ -0,0 +1,36 @@
+﻿using DebtDiary.Core;
+using System.Collections.Generic;
+
+namespace DebtDiary
+{
+    public class DebtorsListDesignModel : DebtorsListViewModel
+    {
+        public static DebtorsListDesignModel Instance => new DebtorsListDesignModel();
+
+        public DebtorsListDesignModel()
+        {
+            Debtors = new List<DebtorsListItemViewModel>
+            {
+                new DebtorsListItemViewModel
+                {
+                    FullName = "Kamil Jaworski",
+                    Initials = "KJ",
+                    Debt = -190.00m,
+                    AvatarColor = AvatarColor.Green
+               },
+               new DebtorsListItemViewModel
+                {
+                    FullName = "Jan Kowalski",
+                    Initials = "JK",
+                    Debt = 200.00m,
+                    AvatarColor = AvatarColor.Orange
+               }
+            };
+        }
+
+
+
+
+    }
+}
+
