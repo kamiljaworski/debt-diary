1bde9e92351e124db7cd417c555c4a7d49666e36
Kamil Jaworski <kamilj610@gmail.com>
2018-08-23 20:08:52 +0200

    Add SideMenuViewModel and its design model

---
+++ DebtDiary/ViewModels/UserControls/SideMenu/Design/SideMenuDesignModel.cs
@@ -0,0 +1,14 @@
+﻿namespace DebtDiary
+{
+    public class SideMenuDesignModel : SideMenuViewModel
+    {
+        public static SideMenuDesignModel Instance => new SideMenuDesignModel();
+
+        public SideMenuDesignModel()
+        {
+            FullName = "Kamil Jaworski";
+            Username = "kamilj610";
+            Initials = "KJ";
+        }
+    }
+}
---
+++ DebtDiary/ViewModels/UserControls/SideMenu/SideMenuViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace DebtDiary
+{
+    public class SideMenuViewModel : BaseViewModel
+    {
+        public string FullName { get; set; }
+
+        public string Username { get; set; }
+
+        public string Initials { get; set; }
+    }
+}
