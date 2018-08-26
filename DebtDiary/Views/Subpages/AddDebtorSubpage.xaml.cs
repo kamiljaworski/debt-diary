30288da5d96db09e4bfe4b2ff950c98cee46b640
Kamil Jaworski <kamilj610@gmail.com>
2018-08-26 15:07:47 +0200

    Add AddDebtorSubpage subpage

---
+++ DebtDiary/Views/Subpages/AddDebtorSubpage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace DebtDiary
+{
+    /// <summary>
+    /// Interaction logic for AddDebtorSubpage.xaml
+    /// </summary>
+    public partial class AddDebtorSubpage : Page
+    {
+        public AddDebtorSubpage()
+        {
+            InitializeComponent();
+        }
+    }
+}
