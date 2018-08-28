fb2f7e699196a07a7a18ba57754e57281b1e87fa
Kamil Jaworski <kamilj610@gmail.com>
2018-08-28 19:12:53 +0200

    Add AddDebtorSubpageViewModel

---
+++ DebtDiary/ViewModels/Subpages/AddDebtorSubpageViewModel.cs
@@ -0,0 +1,25 @@
+﻿using DebtDiary.Core;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace DebtDiary
+{
+    public class AddDebtorSubpageViewModel : BaseViewModel
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public Gender Gender { get; set; } = Gender.None;
+
+        public ICommand AddDebtorCommand { get; set; }
+
+        public AddDebtorSubpageViewModel()
+        {
+            AddDebtorCommand = new RelayCommand(async () => await AddDebtorAsync());
+        }
+
+        private async Task AddDebtorAsync()
+        {
+            return;
+        }
+    }
+}
