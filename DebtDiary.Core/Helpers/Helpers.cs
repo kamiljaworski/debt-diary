1230c921e89a52aaed258ca34d4c0788ebd8b83e
Kamil Jaworski <kamilj610@gmail.com>
2018-08-29 19:12:21 +0200

    Move Initials method to Helpers class

---
+++ DebtDiary.Core/Helpers/Helpers.cs
@@ -0,0 +1,30 @@
+﻿using System.Linq;
+using System.Text;
+
+namespace DebtDiary.Core
+{
+    public static class Helpers
+    {
+        /// <summary>
+        /// Get initials from first and last name
+        /// </summary>
+        /// <param name="firstName">First name of a person</param>
+        /// <param name="lastName">Last name of a person</param>
+        /// <returns>Person initials</returns>
+        public static string GetInitials(string firstName, string lastName)
+        {
+            // Check if first and last name has at least 1 character
+            if (firstName == null || firstName.Count() == 0)
+                return string.Empty;
+
+            if (lastName == null || lastName.Count() == 0)
+                return string.Empty;
+
+            // Build Initials
+            StringBuilder stringBuilder = new StringBuilder();
+            stringBuilder.Append(firstName[0], 1);
+            stringBuilder.Append(lastName[0], 1);
+            return stringBuilder.ToString().ToUpper();
+        }
+    }
+}
--- DebtDiary.Core/Models/Person.cs
+++ DebtDiary.Core/Models/Person.cs
@@ -20,24 +20,7 @@ namespace DebtDiary.Core
 
         public AvatarColor AvatarColor { get; set; } = AvatarColor.Green;
 
-        public string Initials
-        {
-            get
-            {
-                // Check if first and last name has at least 1 character
-                if (FirstName == null || FirstName.Count() == 0)
-                    return string.Empty;
-
-                if (LastName == null || LastName.Count() == 0)
-                    return string.Empty;
-
-                // Build Initials
-                StringBuilder stringBuilder = new StringBuilder();
-                stringBuilder.Append(FirstName[0], 1);
-                stringBuilder.Append(LastName[0], 1);
-                return stringBuilder.ToString().ToUpper();
-            }
-        }
+        public string Initials => Helpers.GetInitials(FirstName, LastName);
 
         public string FullName => $"{FirstName} {LastName}";
     }
