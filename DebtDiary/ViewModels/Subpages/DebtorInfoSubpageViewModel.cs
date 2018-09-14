23978ad740535db2608a8a126d5fd5724536e114
Kamil Jaworski <kamilj610@gmail.com>
2018-09-14 19:04:47 +0200

    Add DebtorInfoSubpageViewModel

---
+++ DebtDiary/ViewModels/Subpages/DebtorInfoSubpageViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace DebtDiary
+{
+    public class DebtorInfoSubpageViewModel : BaseViewModel
+    {
+        public string FullName { get; set; }
+
+        public DebtorInfoSubpageViewModel()
+        {
+            IApplicationViewModel applicationViewModel = IocContainer.Get<IApplicationViewModel>();
+            FullName = applicationViewModel.SelectedDebtor?.FullName;
+        }
+    }
+}
