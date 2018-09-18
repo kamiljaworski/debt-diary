9638627ee51d7499918621b243ad72b8c769de2a
Kamil Jaworski <kamilj610@gmail.com>
2018-09-18 20:20:35 +0200

    Add ShortOperationsList ViewModels

---
+++ DebtDiary/ViewModels/UserControls/OperationsList/ShortOperationsListItemViewModel.cs
@@ -0,0 +1,21 @@
+﻿using DebtDiary.Core;
+
+namespace DebtDiary
+{
+    public class ShortOperationsListItemViewModel : BaseViewModel
+    {
+        public decimal Value { get; set; }
+        public string Description { get; set; }
+
+        public ShortOperationsListItemViewModel()
+        {
+
+        }
+
+        public ShortOperationsListItemViewModel(Operation operation)
+        {
+            Value = operation.Value;
+            Description = operation.Description;
+        }
+    }
+}
---
+++ DebtDiary/ViewModels/UserControls/OperationsList/ShortOperationsListViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.ObjectModel;
+
+namespace DebtDiary
+{
+    public class ShortOperationsListViewModel
+    {
+        public ObservableCollection<DebtorsListItemViewModel> Operations { get; set; } = new ObservableCollection<DebtorsListItemViewModel>();
+
+    }
+}
