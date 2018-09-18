765954521a00a86811e25d6ba7689165658f6225
Kamil Jaworski <kamilj610@gmail.com>
2018-09-18 19:55:56 +0200

    Add ShortOperationsListItem UserControl

---
+++ DebtDiary/UserControls/OperationsList/ShortOperationsListItem.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace DebtDiary
+{
+    /// <summary>
+    /// Interaction logic for ShortOperationsListItem.xaml
+    /// </summary>
+    public partial class ShortOperationsListItem : UserControl
+    {
+        public ShortOperationsListItem()
+        {
+            InitializeComponent();
+        }
+    }
+}
