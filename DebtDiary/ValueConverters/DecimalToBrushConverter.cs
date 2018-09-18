6195218bafaa68cf238e95b350976225f169687b
Kamil Jaworski <kamilj610@gmail.com>
2018-09-18 20:38:48 +0200

    Change DecimalToBrushConverter to support operations

--- DebtDiary/ValueConverters/DecimalToBrushConverter.cs
+++ DebtDiary/ValueConverters/DecimalToBrushConverter.cs
@@ -15,14 +15,24 @@ namespace DebtDiary
                 return Brushes.LightGreen;
 
             decimal debt = (decimal)value;
+            string valueParameter = (string)parameter;
 
+            // If parameter is equal to "Operation" find appropriate darker color
+            if (valueParameter != null && valueParameter == "Operation")
+            {
+                if (debt > 0)
+                    return Brushes.Green;
+                else if (debt < 0)
+                    return Brushes.Red;
+            }
+
+            // Find appropriate color
             if (debt > 0)
                 return Brushes.LightGreen;
             else if (debt < 0)
                 return Brushes.Coral;
             else
                 return Brushes.LightGoldenrodYellow;
-
         }
 
         public override object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) => throw new NotImplementedException();
