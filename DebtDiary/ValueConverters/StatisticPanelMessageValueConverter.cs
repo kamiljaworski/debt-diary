7f6fcdf9e4dd2b9a7974dc6f0fc7af8b9afa35fc
Kamil Jaworski <kamilj610@gmail.com>
2018-09-25 20:01:33 +0200

    Add converter whitch removes '-' sign when there is negative value in debt panel

---
+++ DebtDiary/ValueConverters/StatisticPanelMessageValueConverter.cs
@@ -0,0 +1,38 @@
+﻿using DebtDiary.Core;
+using System;
+using System.Globalization;
+using System.Linq;
+using System.Reflection;
+using System.Resources;
+
+namespace DebtDiary
+{
+    /// <summary>
+    /// Removes '-' when there is negative value
+    /// </summary>
+    public class StatisticPanelMessageValueConverter : BaseMultiValueConverter<StatisticPanelMessageValueConverter>
+    {
+        public override object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            // Check if values are correct
+            if (values == null || values.Count() < 2)
+                return string.Empty;
+
+            if (!(values[0] is StatisticPanelMessage))
+                return string.Empty;
+
+            // Get values
+            StatisticPanelMessage message = (StatisticPanelMessage)values[0];
+            string panelValue = values[1] is string ? (string)values[1] : null;
+
+            if (message == StatisticPanelMessage.DebtFemale || message == StatisticPanelMessage.DebtMale)
+                if (panelValue != null && panelValue.Length > 0 && panelValue[0] == '-')
+                    return panelValue.Remove(0, 1);
+
+            return panelValue;
+        }
+
+
+        public override object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture) => throw new NotImplementedException();
+    }
+}
