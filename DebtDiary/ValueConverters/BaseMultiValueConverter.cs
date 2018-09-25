0ed035b13ac8f06d485f49a92bff745cd726f467
Kamil Jaworski <kamilj610@gmail.com>
2018-09-25 19:22:19 +0200

    Add BaseMultiConverter and change color binding

---
+++ DebtDiary/ValueConverters/BaseMultiValueConverter.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Markup;
+
+namespace DebtDiary
+{
+    /// <summary>
+    /// A base multi value converter class that allows XAML usage
+    /// </summary>
+    /// <typeparam name="T">Type of this value converter</typeparam>
+    public abstract class BaseMultiValueConverter<T> : MarkupExtension, IMultiValueConverter where T: class, new()
+    {
+        #region Private Members
+
+        /// <summary>
+        /// Private static instance of the value converter 
+        /// </summary>
+        private static T _converter = null;
+        #endregion
+
+        #region Markup Extension Methods
+
+        /// <summary>
+        /// Provides a static instance of the value converter 
+        /// </summary>
+        /// <param name="serviceProvider">The service provider</param>
+        /// <returns></returns>
+        public override object ProvideValue(IServiceProvider serviceProvider) => _converter ?? (_converter = new T());
+        #endregion
+
+        #region Value Converter Methods
+
+        /// <summary>
+        /// A method used to convert one type to another
+        /// </summary>
+        public abstract object Convert(object[] values, Type targetType, object parameter, CultureInfo culture);
+
+        /// <summary>
+        /// A method used to convert value back to its source type
+        /// </summary>
+        public abstract object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture);
+
+        #endregion
+    }
+}
---
+++ DebtDiary/ValueConverters/StatisticPanelMessageToBrushConverter.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Globalization;
+using System.Linq;
+using System.Windows.Media;
+
+namespace DebtDiary
+{
+    /// <summary>
+    /// Converts <see cref="StatisticPanelMessage"/> to <see cref="Brush"/> for background color
+    /// </summary>
+    public class StatisticPanelMessageToBrushConverter : BaseMultiValueConverter<StatisticPanelMessageToBrushConverter>
+    {
+        public override object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            // Check if values are correct
+            if (values == null || values.Count() < 2)
+                return Brushes.Green;
+
+            if (!(values[0] is StatisticPanelMessage))
+                return Brushes.Green;
+
+            StatisticPanelMessage message = (StatisticPanelMessage)values[0];
+            string panelValue = values[1] is string ? (string)values[1] : null;
+
+            // Return appropriate color
+            switch (message)
+            {
+                case StatisticPanelMessage.DebtMale:
+                case StatisticPanelMessage.DebtFemale:
+                    if (panelValue != null && panelValue.Length > 0 && panelValue[0] == '-')
+                        return Brushes.OrangeRed.Color;
+                    else
+                        return Brushes.YellowGreen.Color;
+
+                case StatisticPanelMessage.NumberOfOperations:
+                    return Brushes.MediumTurquoise.Color;
+
+                case StatisticPanelMessage.LastOperation:
+                    return Brushes.DeepPink.Color;
+
+                case StatisticPanelMessage.AdditionDate:
+                    return Brushes.Orange.Color;
+            }
+
+            // If there wasn't returned any color return green one
+            return Brushes.Green;
+        }
+
+        public override object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture) => throw new NotImplementedException();
+    }
+}
