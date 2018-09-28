b30024c5418fd95f431b1cda980832a585bdd9ae
Kamil Jaworski <kamilj610@gmail.com>
2018-09-28 18:42:55 +0200

    Add ToUpperFirstLetter string extension

--- DebtDiary.Core/Helpers/StringExtensions.cs
+++ DebtDiary.Core/Helpers/StringExtensions.cs
@@ -16,5 +16,20 @@
 
             return changedString;
         }
+
+        /// <summary>
+        /// Changes first letter of a string to upper
+        /// </summary>
+        public static string ToUpperFirstLetter(this string s)
+        {
+            if (string.IsNullOrEmpty(s))
+                return s;
+
+            string changedString = s[0].ToString();
+            changedString = changedString.ToUpper();
+            changedString += s.Remove(0, 1);
+
+            return changedString;
+        }
     }
 }
