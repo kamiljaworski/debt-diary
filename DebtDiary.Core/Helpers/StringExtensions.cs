718046758d64b145c0e664c19515b4441c005631
Kamil Jaworski <kamilj610@gmail.com>
2018-09-28 18:38:45 +0200

    Add string extension method ToLowerFirstLetter

---
+++ DebtDiary.Core/Helpers/StringExtensions.cs
@@ -0,0 +1,21 @@
+﻿namespace DebtDiary.Core
+{
+    public static class StringExtensions
+    {
+        /// <summary>
+        /// Changes first letter of a string to lower
+        /// </summary>
+        private static string ToLowerFirstLetter(this string s)
+        {
+            if (string.IsNullOrEmpty(s))
+                return s;
+
+            string changedString = s[0].ToString();
+            changedString = changedString.ToLower();
+            changedString += s.Remove(0, 1);
+
+            return changedString;
+        }
+
+    }
+}
