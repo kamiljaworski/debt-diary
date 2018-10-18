4206f6d91af09e6c3db0fa09dd7249c73612c12a
Kamil Jaworski <kamilj610@gmail.com>
2018-10-18 10:10:40 +0200

    Add OperationListItem user control

---
+++ DebtDiary/UserControls/OperationsList/OperationsListItem.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace DebtDiary
+{
+    /// <summary>
+    /// Interaction logic for ShortOperationsListItem.xaml
+    /// </summary>
+    public partial class OperationsListItem : UserControl
+    {
+        public OperationsListItem()
+        {
+            InitializeComponent();
+        }
+    }
+}
