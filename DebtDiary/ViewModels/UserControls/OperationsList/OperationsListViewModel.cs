748045a49108dddc303d5f4c245d6f9e5d059d14
Kamil Jaworski <kamilj610@gmail.com>
2018-10-18 10:15:44 +0200

    Add OperationsListViewModel

---
+++ DebtDiary/ViewModels/UserControls/OperationsList/OperationsListViewModel.cs
@@ -0,0 +1,26 @@
+﻿using DebtDiary.Core;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+
+namespace DebtDiary
+{
+    public class OperationsListViewModel
+    {
+        public ObservableCollection<OperationsListItemViewModel> Operations { get; set; } = new ObservableCollection<OperationsListItemViewModel>();
+        public bool IsAnyOperationAdded => Operations.Count > 0 ? true : false;
+
+        public OperationsListViewModel()
+        {
+        }
+
+        public OperationsListViewModel(IList<Operation> operations)
+        {
+            foreach (Operation operation in operations)
+                Operations.Add(new OperationsListItemViewModel(operation));
+
+            Operations = new ObservableCollection<OperationsListItemViewModel>(Operations.OrderByDescending(x => x.OperationDate));
+        }
+
+    }
+}
