4aa1919be4e614c13a3a4edd8f6fb091ac80958f
Kamil Jaworski <kamilj610@gmail.com>
2018-10-18 10:12:11 +0200

    Add OperationsListItemViewModel

---
+++ DebtDiary/ViewModels/UserControls/OperationsList/OperationsListItemViewModel.cs
@@ -0,0 +1,28 @@
+﻿using DebtDiary.Core;
+using System;
+
+namespace DebtDiary
+{
+    public class OperationsListItemViewModel : BaseViewModel
+    {
+        public string FullName { get; set; }
+        public decimal Value { get; set; }
+        public string FormattedValue => Helpers.GetFormattedCurrency(Value);
+        public string Description { get; set; }
+        public DateTime OperationDate { get; set; }
+        public string FormattedOperationDate => OperationDate.ToShortDateString();
+
+
+        public OperationsListItemViewModel()
+        {
+        }
+
+        public OperationsListItemViewModel(Operation operation)
+        {
+            FullName = operation.Debtor.FullName;
+            Value = operation.Value;
+            Description = operation.Description.ToLowerFirstLetter();
+            OperationDate = operation.AdditionDate;
+        }
+    }
+}
