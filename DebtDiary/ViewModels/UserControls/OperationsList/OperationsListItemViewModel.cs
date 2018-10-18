d013751a312551394292c992e00bbf0d7b6fb48e
Kamil Jaworski <kamilj610@gmail.com>
2018-10-18 10:17:24 +0200

    Add OperationsListItemViewModel inheritance from short one

--- DebtDiary/ViewModels/UserControls/OperationsList/OperationsListItemViewModel.cs
+++ DebtDiary/ViewModels/UserControls/OperationsList/OperationsListItemViewModel.cs
@@ -3,26 +3,18 @@ using System;
 
 namespace DebtDiary
 {
-    public class OperationsListItemViewModel : BaseViewModel
+    public class OperationsListItemViewModel : ShortOperationsListItemViewModel
     {
         public string FullName { get; set; }
-        public decimal Value { get; set; }
-        public string FormattedValue => Helpers.GetFormattedCurrency(Value);
-        public string Description { get; set; }
-        public DateTime OperationDate { get; set; }
-        public string FormattedOperationDate => OperationDate.ToShortDateString();
 
 
-        public OperationsListItemViewModel()
+        public OperationsListItemViewModel() : base()
         {
         }
 
-        public OperationsListItemViewModel(Operation operation)
+        public OperationsListItemViewModel(Operation operation) : base(operation)
         {
             FullName = operation.Debtor.FullName;
-            Value = operation.Value;
-            Description = operation.Description.ToLowerFirstLetter();
-            OperationDate = operation.AdditionDate;
         }
     }
 }
