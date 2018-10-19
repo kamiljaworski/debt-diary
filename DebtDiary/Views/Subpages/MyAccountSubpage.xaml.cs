2d2cbf08f1a33d58df055b865eab87c5c5ff999e
Kamil Jaworski <kamilj610@gmail.com>
2018-10-19 14:08:22 +0200

    Add MyAccountSubpage to the project

---
+++ DebtDiary/Views/Subpages/MyAccountSubpage.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows.Controls;
+
+namespace DebtDiary
+{
+    /// <summary>
+    /// Interaction logic for MyAccountSubpage.xaml
+    /// </summary>
+    public partial class MyAccountSubpage : Page
+    {
+        public MyAccountSubpage()
+        {
+            DataContext = new AddDebtorSubpageViewModel();
+            InitializeComponent();
+        }
+    }
+}
