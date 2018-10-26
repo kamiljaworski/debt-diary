b496b0ec31389c7d9865c0883d4a06e60e4ea29b
Kamil Jaworski <kamilj610@gmail.com>
2018-10-27 00:15:20 +0200

    Add BooleanToDoubleConverter and use it in main window BlurEffect

---
+++ DebtDiary/ValueConverters/BooleanToDoubleConverter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+
+namespace DebtDiary
+{
+    /// <summary>
+    /// Converts <see cref="Boolean"/> to <see cref="double"/> for use in window blur effect
+    /// true = 5.0
+    /// false = 0.0
+    /// </summary>
+    public class BooleanToDoubleConverter : BaseValueConverter<BooleanToDoubleConverter>
+    {
+        public override object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (!(value is Boolean))
+                return null;
+
+            Boolean boolValue = (Boolean)value;
+
+            if (boolValue == true)
+                return 5.0;
+
+            return 0.0;
+        }
+
+        public override object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) => throw new NotImplementedException();
+    }
+}
