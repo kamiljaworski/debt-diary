7723377ea8e81711a953efec236c35483595a990
Kamil Jaworski <kamilj610@gmail.com>
2018-10-27 00:08:59 +0200

    Add DialogFacade to ViewModelLocator

--- DebtDiary/ViewModels/ViewModelLocator.cs
+++ DebtDiary/ViewModels/ViewModelLocator.cs
@@ -1,13 +1,8 @@
 ﻿namespace DebtDiary
 {
-    /// <summary>
-    /// Locates View Models from IoC for use in xaml binding
-    /// </summary>
     public class ViewModelLocator
     {
-        /// <summary>
-        /// ApplicationViewModel Property
-        /// </summary>
         public IApplicationViewModel ApplicationViewModel { get => IocContainer.Get<IApplicationViewModel>(); }
+        public IDialogFacade DialogFacade { get => IocContainer.Get<IDialogFacade>(); }
     }
 }
