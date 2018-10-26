2b5a9696d3943a70d1fe66309fb4a29e9cf3513c
Kamil Jaworski <kamilj610@gmail.com>
2018-10-26 23:48:43 +0200

    Add ApplicationPageProperty and replace dark&light control buttons style with universal one

---
+++ DebtDiary/AttachedProperties/ApplicationPageProperty.cs
@@ -0,0 +1,9 @@
+﻿namespace DebtDiary
+{
+    /// <summary>
+    /// ApplicationPage attached property used in window control buttons
+    /// </summary>
+    class ApplicationPageProperty : BaseAttachedProperty<ApplicationPageProperty, ApplicationPage>
+    {
+    }
+}
