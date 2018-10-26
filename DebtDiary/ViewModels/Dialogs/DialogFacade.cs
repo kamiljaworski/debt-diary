e9106828db062d27b6991a1e3de5413ddbf3afa2
Kamil Jaworski <kamilj610@gmail.com>
2018-10-27 00:08:15 +0200

    Add IsDialogOpened property to DialogFacade

--- DebtDiary/ViewModels/Dialogs/DialogFacade.cs
+++ DebtDiary/ViewModels/Dialogs/DialogFacade.cs
@@ -4,11 +4,15 @@
     {
         public DialogMessage DialogMessage { get; private set; }
 
+        public bool IsDialogOpened { get; private set; } = false;
+
         public void OpenDialog(DialogMessage dialogMessage)
         {
+            IsDialogOpened = true;
             DialogMessage = dialogMessage;
             DialogWindow win = new DialogWindow();
             win.ShowDialog();
+            IsDialogOpened = false;
         }
     }
 }
--- DebtDiary/ViewModels/Dialogs/IDialogFacade.cs
+++ DebtDiary/ViewModels/Dialogs/IDialogFacade.cs
@@ -8,6 +8,8 @@
         /// <param name="dialogMessage"><see cref="DialogMessage"/> you want to show</param>
         void OpenDialog(DialogMessage dialogMessage);
 
+        bool IsDialogOpened { get; }
+
         /// <summary>
         /// Dialog Message of an actual dialog window
         /// </summary>
