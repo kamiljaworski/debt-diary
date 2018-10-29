4f00a054540374649397ca7a2b57955bdf8a132d
Kamil Jaworski <kamilj610@gmail.com>
2018-10-29 20:33:37 +0100

    Remove unnecessary code in DebtorInfoSubpage.xaml.cs

--- DebtDiary/Views/Subpages/DebtorInfoSubpage.xaml.cs
+++ DebtDiary/Views/Subpages/DebtorInfoSubpage.xaml.cs
@@ -6,30 +6,14 @@ using System.Windows.Controls;
 namespace DebtDiary
 {
     /// <summary>
-    /// Interaction logic for SummarySubpage.xaml
+    /// Interaction logic for DebtorInfoSubpage.xaml
     /// </summary>
     public partial class DebtorInfoSubpage : Page
     {
-        public SeriesCollection SeriesCollection { get; set; }
-        public string[] Labels { get; set; }
-        public Func<double, string> YFormatter { get; set; }
-
         public DebtorInfoSubpage()
         {
-            InitializeComponent();
-
-            SeriesCollection = new SeriesCollection
-            {
-                new LineSeries
-                {
-                    Title = "Test",
-                    Values = new ChartValues<double> { -1000, -700, 0, -200 , 0, 200, 500, 100, 0, 290 }
-                }
-            };
-
-            YFormatter = value => value.ToString("C");
-
             DataContext = IocContainer.Get<IDebtorInfoSubpageViewModel>();
+            InitializeComponent();
         }
     }
 }
