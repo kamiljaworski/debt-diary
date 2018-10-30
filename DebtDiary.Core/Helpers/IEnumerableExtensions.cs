4651236913794736c4cd3cd9c84010397607f924
Kamil Jaworski <kamilj610@gmail.com>
2018-10-30 19:16:07 +0100

    Add LINQ style ForEach for IEnumerable

---
+++ DebtDiary.Core/Helpers/IEnumerableExtensions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DebtDiary.Core
+{
+    public static class IEnumerableExtensions
+    {
+        public static void ForEach<T>(this IEnumerable<T> enumeration, Action<T> action)
+        {
+            foreach (T item in enumeration)
+                action(item);
+        }
+    }
+}
