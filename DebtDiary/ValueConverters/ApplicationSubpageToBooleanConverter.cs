50b82e8edf4fcb4a2c800b6b59480750358dcfd3
Kamil Jaworski <kamilj610@gmail.com>
2018-11-06 20:49:10 +0100

    Add ApplicationSubpageToBooleanConverter

---
+++ DebtDiary/ValueConverters/ApplicationSubpageToBooleanConverter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Globalization;
+
+namespace DebtDiary
+{
+    /// <summary>
+    /// Converts <see cref="ApplicationSubpage"/> to <see cref="Boolean"/> used for binding IsSelected button property
+    /// </summary>
+    public class ApplicationSubpageToBooleanConverter : BaseValueConverter<ApplicationSubpageToBooleanConverter>
+    {
+        public override object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (!(value is ApplicationSubpage))
+                return false;
+
+            if (parameter == null && !(parameter is string))
+                return false;
+
+            ApplicationSubpage subpage = (ApplicationSubpage)value;
+            string buttonSubpage = (string)parameter;
+
+            if (subpage.ToString() == buttonSubpage)
+                return true;
+
+            return false;
+        }
+
+        public override object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) => throw new NotImplementedException();
+    }
+}
