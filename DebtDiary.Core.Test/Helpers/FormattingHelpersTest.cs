3a6783e5b1f27b42752e60019f800976340d5f57
Kamil Jaworski <kamilj610@gmail.com>
2018-11-20 19:20:47 +0100

    Add TestGetInitialsWithValidData unit test

---
+++ DebtDiary.Core.Test/Helpers/FormattingHelpersTest.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+
+namespace DebtDiary.Core.Test
+{
+    [TestFixture]
+    public class FormattingHelpersTest
+    {
+        [TestCase("Jan", "Kowalski", "JK")]
+        [TestCase("Morgan", "Freeman", "MF")]
+        [TestCase("Łukasz", "Stanisławowski", "ŁS")]
+        public void TestGetInitialsWithValidData(string firstName, string lastName, string expectedValue)
+        {
+            string actualValue = FormattingHelpers.GetInitials(firstName, lastName);
+            Assert.AreEqual(expectedValue, actualValue);
+        }
+    }
+}
