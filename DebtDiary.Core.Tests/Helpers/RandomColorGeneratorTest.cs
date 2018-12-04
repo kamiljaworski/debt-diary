9c19af91db39a2ce2667bc8666fe6c2795df9091
Kamil Jaworski <kamilj610@gmail.com>
2018-12-04 20:46:00 +0100

    Add RandomColorGenerator class tests

---
+++ DebtDiary.Core.Tests/Helpers/RandomColorGeneratorTest.cs
@@ -0,0 +1,30 @@
+﻿using NUnit.Framework;
+using System.Collections.Generic;
+
+namespace DebtDiary.Core.Tests
+{
+    [TestFixture]
+    public class RandomColorGeneratorTest
+    {
+        [Test]
+        public void TestGetRandomColor()
+        {
+            Color firstColor = RandomColorGenerator.GetRandomColor();
+            Color secondColor = RandomColorGenerator.GetRandomColor();
+
+            Assert.AreNotEqual(firstColor, secondColor);
+        }
+
+        [Test]
+        public void TestGetRandomColorExcept()
+        {
+            Color skippedColor = Color.LightSkyBlue;
+            IList<Color> randomColors = new List<Color>();
+
+            for (int i = 0; i < 1000; ++i)
+                randomColors.Add(RandomColorGenerator.GetRandomColorExcept(skippedColor));
+
+            Assert.IsFalse(randomColors.Contains(skippedColor));
+        }
+    }
+}
--- DebtDiary.Core/Helpers/RandomColorGenerator.cs
+++ DebtDiary.Core/Helpers/RandomColorGenerator.cs
@@ -18,7 +18,5 @@ namespace DebtDiary.Core
 
             return color;
         }
-
-
     }
 }
