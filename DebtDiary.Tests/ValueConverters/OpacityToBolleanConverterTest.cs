cca181fc99acf998fecfe2e7cf0f478f97821d8a
Kamil Jaworski <kamilj610@gmail.com>
2018-12-19 20:56:08 +0100

    Add OpacityToBollean value converter tests

---
+++ DebtDiary.Tests/ValueConverters/OpacityToBolleanConverterTest.cs
@@ -0,0 +1,32 @@
+﻿using NUnit.Framework;
+using System;
+using System.Windows;
+
+namespace DebtDiary.Tests.ValueConverters
+{
+    [TestFixture]
+    public class OpacityToBolleanConverterTest
+    {
+        [TestCase(1.0, true)]
+        [TestCase(0.900001, true)]
+        [TestCase(0.9, false)]
+        [TestCase("as", false)]
+        [TestCase("", false)]
+        [TestCase(null, false)]
+        public void TestConvert(object value, bool expectedResult)
+        {
+            OpacityToBolleanConverter converter = new OpacityToBolleanConverter();
+            bool result = (bool)converter.Convert(value, null, null, null);
+
+            Assert.AreEqual(expectedResult, result);
+        }
+
+        [Test]
+        public void TestConvertBack()
+        {
+            OpacityToBolleanConverter converter = new OpacityToBolleanConverter();
+
+            Assert.Throws<NotImplementedException>(() => converter.ConvertBack(null, null, null, null));
+        }
+    }
+}
