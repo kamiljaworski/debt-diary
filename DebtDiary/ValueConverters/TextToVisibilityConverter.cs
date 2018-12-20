c9eaf36c2d0681a758dcfea3cbda5827e0c37ddc
Kamil Jaworski <kamilj610@gmail.com>
2018-12-20 21:19:23 +0100

    Add TextToVisibility value converter tests and refactor it

--- DebtDiary/ValueConverters/TextToVisibilityConverter.cs
+++ DebtDiary/ValueConverters/TextToVisibilityConverter.cs
@@ -5,14 +5,14 @@ using System.Windows;
 namespace DebtDiary
 {
     /// <summary>
-    /// Converts <see cref="String"/> from a TextBox to <see cref="Visibility"/>
+    /// Converts <see cref="string"/> from a TextBox to <see cref="Visibility"/>
     /// </summary>
     public class TextToVisibilityConverter : BaseValueConverter<TextToVisibilityConverter>
     {
         public override object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
             // If string is null or empty return visible
-            if (value is String && String.IsNullOrEmpty((value as String)))
+            if (string.IsNullOrEmpty(value as string))
                 return Visibility.Visible;
 
             // If not return Hidden
