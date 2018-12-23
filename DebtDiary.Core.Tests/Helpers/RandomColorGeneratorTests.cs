c1af4736710bfe70bf3bad70cf00bdb970eba52f
Kamil Jaworski <kamilj610@gmail.com>
2018-12-23 18:29:32 +0100

    Refactor RandomColorGeneratorTests->TestGetRandomColor assertion to be more readable

--- DebtDiary.Core.Tests/Helpers/RandomColorGeneratorTests.cs
+++ DebtDiary.Core.Tests/Helpers/RandomColorGeneratorTests.cs
@@ -14,8 +14,9 @@ namespace DebtDiary.Core.Tests
 
             for (int i = 0; i < 1000; ++i)
                 randomColors.Add(RandomColorGenerator.GetRandomColor());
+            var colorsDifferentFromFirstOne = randomColors.Select(x => x != randomColors[0]);
 
-            Assert.That(randomColors.Select(x => x != randomColors[0]).Count() > 0);
+            Assert.True(colorsDifferentFromFirstOne.Count() > 0);
         }
 
         [Test]
@@ -27,7 +28,7 @@ namespace DebtDiary.Core.Tests
             for (int i = 0; i < 1000; ++i)
                 randomColors.Add(RandomColorGenerator.GetRandomColorExcept(skippedColor));
 
-            Assert.IsFalse(randomColors.Contains(skippedColor));
+            Assert.False(randomColors.Contains(skippedColor));
         }
     }
 }
