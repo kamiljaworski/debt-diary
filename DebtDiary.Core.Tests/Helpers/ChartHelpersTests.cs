260422dae0ad8ee42524f7e94353e53d6a0add47
Kamil Jaworski <kamilj610@gmail.com>
2019-01-07 23:52:39 +0100

    Add one ChartHelpers test

--- DebtDiary.Core.Tests/Helpers/ChartHelpersTests.cs
+++ DebtDiary.Core.Tests/Helpers/ChartHelpersTests.cs
@@ -60,5 +60,26 @@ namespace DebtDiary.Core.Tests
 
             Assert.AreEqual(expectedEnumeration, actualEnumeration);
         }
+
+        [Test]
+        public void TestGetChartPointsWithFirstOperationOlderThanStartDate()
+        {
+            DateTime startDate = DateTime.Today - TimeSpan.FromDays(10);
+            IList<Operation> operations = new List<Operation>
+            {
+                new Operation { AdditionDate = startDate - TimeSpan.FromDays(2), Value = 100.0m },
+                new Operation { AdditionDate = startDate, Value = 200.0m },
+                new Operation { AdditionDate = startDate + TimeSpan.FromDays(1), Value = -50.0m },
+                new Operation { AdditionDate = startDate + TimeSpan.FromDays(2), Value = -100.0m },
+                new Operation { AdditionDate = startDate + TimeSpan.FromDays(2), Value = -500.50m },
+                new Operation { AdditionDate = startDate + TimeSpan.FromDays(5), Value = 600.0m },
+                new Operation { AdditionDate = startDate + TimeSpan.FromDays(7), Value = 100.0m }
+            };
+
+            IEnumerable<decimal> expectedEnumeration = new List<decimal> { 0.0m, 100.0m, 100.0m, 300.0m, 250.0m, -350.50m, -350.50m, -350.50m, 249.50m, 249.50m, 349.50m, 349.50m, 349.50m, 349.50m };
+            IEnumerable<decimal> actualEnumeration = operations.GetChartPoints(startDate);
+
+            Assert.AreEqual(expectedEnumeration, actualEnumeration);
+        }
     }
 }
